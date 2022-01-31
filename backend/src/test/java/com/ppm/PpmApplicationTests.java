fdb328c604cff800fba4f38da05b85231f7a8a8a
umesh1987 <umesh.state@gmail.com>
2022-01-31 11:27:45 -0600

    init commit

---
+++ backend/src/test/java/com/ppm/PpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
